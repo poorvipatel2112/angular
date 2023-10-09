526e0f95f119d62dff8d2be8a0604e5455f59004
Poorvi <poorvip@argusoft.com>
2023-10-09 12:24:22 +0530

    string interpolation completed

--- demo-app/src/app/app.module.ts
+++ demo-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import {SuccessAlertComponent} from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
