b7895d3c61334a905b7794cd8bdbf78f1c43642d
Poorvi <poorvip@argusoft.com>
2023-10-18 11:57:47 +0530

    removed empty folder service and started new project named appointmnet-app

--- appointment-app/src/app/app.component.spec.ts
+++ appointment-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'service-start'`, () => {
+  it(`should have as title 'appointment-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('service-start');
+    expect(app.title).toEqual('appointment-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('service-start app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('appointment-app app is running!');
   });
 });
--- appointment-app/src/app/app.module.ts
+++ appointment-app/src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewAccountComponent } from './new-account/new-account.component';
-import { AccountComponent } from './account/account.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewAccountComponent,
-    AccountComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
