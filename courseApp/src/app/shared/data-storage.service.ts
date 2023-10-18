5fc51ea6efa1687faf69d4b0278e37e9833914f3
Poorvi <poorvip@argusoft.com>
2023-10-18 10:55:18 +0530

    fetch data implemented

--- courseApp/src/app/shared/data-storage.service.ts
+++ courseApp/src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipe/recipe.service";
+import { Recipe } from "../recipe/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -15,4 +17,18 @@ export class DataStorageService{
         })
     }
 
-}
+    fetchRecipes(){
+        this.http.get<Recipe[]>('https://ng-courseapp-a7b25-default-rtdb.firebaseio.com/recipes.json')
+        .pipe(
+            map(recipes =>{
+                return recipes.map(recipe =>{
+                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+                });
+            })
+        )
+        .subscribe(recipes =>{
+            this.recipeService.setRecipes(recipes);
+        })
+    }
+
+} 
